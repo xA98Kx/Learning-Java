a0c20811ebbeb3af47518efa9a614246053d448e
A98K <116504369+xA98Kx@users.noreply.github.com>
2022-11-13 09:36:39 +0530

    New forloop program

---
+++ switch_case/TenToFive_Divi3.java
@@ -0,0 +1,13 @@
+class TenToFive_Divi3
+{
+    public static void main(String[] args) {
+        int i, n;
+        for(i = 10; i <= 100; i++)
+        {
+            if (i%3 == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
