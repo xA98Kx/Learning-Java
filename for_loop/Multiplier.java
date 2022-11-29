f6bd8f6baf7d53e06bac564fceea9ad6b18a6fc7
A98K <116504369+xA98Kx@users.noreply.github.com>
2022-11-29 23:34:59 +0530

    fuix

--- for_loop/Multiplier.java
+++ for_loop/Multiplier.java
@@ -6,12 +6,12 @@ public class Multiplier {
         int c=0, n, f=0;
         System.out.println("Enter number");
         n = x.nextInt();
+        x.close();
         for (c = 0; c <= 10;c++)
         {
             f = n*c;
             System.out.println(n+"*"+c+"="+f);
         }
-        x.close();
         
     }
     
