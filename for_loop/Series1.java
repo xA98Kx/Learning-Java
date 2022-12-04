e39f4d440725853f8f4ec12c5218f718f2631dd4
A98K <116504369+xA98Kx@users.noreply.github.com>
2022-12-04 16:48:37 +0530

    ok

---
+++ for_loop/Series1.java
@@ -0,0 +1,12 @@
+//import java.util.Scanner;
+
+public class Series1 {
+    public static void main(String[] args) {
+        //Scanner x = new Scanner(System.in);
+
+        for (int i =1; i <= 10; i++)
+        {
+            System.out.print(i+",");
+        }
+    }
+}
