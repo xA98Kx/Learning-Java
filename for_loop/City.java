82688e6e334666fa04cc3e287a25b4dfb55ee0ff
A98K <116504369+xA98Kx@users.noreply.github.com>
2024-01-14 16:50:48 +0530

    somemore changes

--- for_loop/City.java
+++ for_loop/City.java
@@ -4,22 +4,29 @@ class City
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         String a[] = new String[5];
+        System.out.println("Enter 4 city names");
         for(int i = 0; i<5; i++)
         {
             a[i] = x.nextLine();
         }
         for(int i=0; i<4; i++)
         {
-            if(a[i+1].length() < a[i].length()){
-                String temp = a[i];
-                a[i] = a[i+1];
-                a[i+1] = temp;
+            for(int j = 0; j<4-i; j++)
+            {
+                if(a[j+1].length() < a[j].length()){
+                    String temp = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = temp;
+                }
             }
 
+            
         }
+        System.out.println("Sorted ");
         for(int i = 0; i<5; i++)
         {
             System.out.println(a[i]);
         }
+        x.close();
     }
 }
--- for_loop/ClassTEacher.java
+++
@@ -1,6 +0,0 @@
-public class ClassTEacher {
-    public static void main(String[] args) {
-        int DDA[][] = new int[40][5];
-        
-    }
-}
