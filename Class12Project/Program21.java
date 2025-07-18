26b5bb3f4e868f116226497ebd4abeb19e19c344
xA98Kx <xa98kx@gmail.com>
2025-07-18 20:40:53 +0530

    removed mango for any user input

--- Class12Project/Program21.java
+++ Class12Project/Program21.java
@@ -1,18 +1,18 @@
+import java.util.Scanner;
+
 public class Program21 {
-    static int n = 0;
-    static String recurringPrint(String s)
-    {
-        if(s.length()!=0){
-        System.out.println(s);
-        n++;
-        return recurringPrint(s.substring(0,4-n));
-        }
-        else
-        {
-            return "";
+    static void recurringPrint(String s, int length, int n) {
+        if (s.length() != 0) {
+            System.out.println(s);
+            recurringPrint(s.substring(0, length - n - 1), length, n + 1);
         }
     }
+
     public static void main(String[] args) {
-        recurringPrint("MANGO");
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter a String to be printed out in a pattern");
+        String in = x.nextLine();
+        System.out.println("The final pattern is \n");
+        recurringPrint(in, in.length(), 0);
     }
 }
